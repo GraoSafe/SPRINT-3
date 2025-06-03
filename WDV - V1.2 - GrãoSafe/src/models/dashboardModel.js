545bf5bfdb0be4ce728221bc7c383987115c7357
AlexTorresOP <alexsander.torres@sptech.school>
2025-06-03 16:57:29 -0300

    mudanças na dash/login/registro

--- WDV - V1.2 - GrãoSafe/src/models/dashboardModel.js
+++ WDV - V1.2 - GrãoSafe/src/models/dashboardModel.js
@@ -2,8 +2,9 @@ var database = require("../database/config")
 
 function temperaturaAtual(fkSensor){
     var instrucao = `
-    select * from medida where fkSensor = 1  ORDER BY dtHora ASC LIMIT 11;
+    select * from medida where fkSensor = 1  ORDER BY dtHora DESC LIMIT 1;
     `
+    console.log('EXECUTANDO:',instrucao)
     return database.executar(instrucao)
 }
 
